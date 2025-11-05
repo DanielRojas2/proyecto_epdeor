9c65eb3c10744b63690ede656e96f056994d7e39
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-11-05 10:58:01 -0400

    cambios en modelo Tomo de la app archivos

--- apps/archivos/models/Tomo.py
+++ apps/archivos/models/Tomo.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Tomo(models.Model):
     ESTADO_CHOICES = (
@@ -8,8 +9,8 @@ class Tomo(models.Model):
     nro_tomo = models.SmallIntegerField(blank=False, null=False)
     titulo = models.CharField(max_length=75, blank=False, null=False)
     glosa = models.TextField(blank=False, null=False)
-    gestion = models.CharField(blank=True, null=True)
-    mes = models.CharField(blank=True, null=True)
+    gestion = models.CharField(max_length=4, blank=True, null=True)
+    mes = models.CharField(max_length=15, blank=True, null=True)
     fecha_apertura = models.DateField(blank=False, null=False)
     estado = models.CharField(max_length=7, default='activo', choices=ESTADO_CHOICES)
     nro_fojas_total = models.SmallIntegerField(default=0, blank=True, null=True)
@@ -23,6 +24,12 @@ class Tomo(models.Model):
                 name='unique_nro_tomo_titulo'
             )
         ]
+
+    def save(self, *args, **kwargs):
+        if self.fecha_apertura:
+            self.gestion = str(self.fecha_apertura.year)
+            self.mes = str(self.fecha_apertura.strftime("%B")).capitalize()
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return f"Tomo: {self.nro_tomo} {self.titulo}"
