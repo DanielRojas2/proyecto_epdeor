548060c7fc1a4a55c4e0f49b52d81d52421f4743
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-11-05 12:44:16 -0400

    cambios en el modelo DetalleTomo

--- apps/archivos/functions/guardar_carpeta.py
+++ apps/archivos/functions/guardar_carpeta.py
@@ -3,5 +3,7 @@ import os
 def get_upload_to(instance, filename):
 
     tomo = instance.tomo.titulo
+    gestion = instance.tomo.gestion
+    mes = instance.tomo.mes
 
-    return os.path.join('archivos', tomo, filename)
+    return os.path.join('archivos', gestion, mes, tomo, filename)
--- apps/archivos/models/DetalleTomo.py
+++ apps/archivos/models/DetalleTomo.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PyPDF2 import PdfReader
 from .Tomo import Tomo
 from ..functions.guardar_carpeta import get_upload_to
 from ..functions.validar_pdf import validate_pdf
@@ -8,7 +9,7 @@ class DetalleTomo(models.Model):
         ('adjunto', 'Adjunto'),
         ('no adjunto', 'No Adjunto')
     )
-    nro_doc = models.SmallIntegerField(default=1, blank=False, null=False)
+    nro_doc = models.SmallIntegerField(blank=False, null=False)
     nombre_archivo = models.CharField(max_length=200, blank=False, null=False)
     fecha_adjunto = models.DateTimeField(auto_now_add=True)
     nro_fojas = models.SmallIntegerField(blank=True, null=True)
@@ -27,10 +28,24 @@ class DetalleTomo(models.Model):
         ]
         
     def save(self, *args, **kwargs):
+        if not self.nro_doc:
+            ultimo_detalle = DetalleTomo.objects.filter(tomo=self.tomo).order_by('-nro_doc').first()
+            self.nro_doc = (ultimo_detalle.nro_doc + 1) if ultimo_detalle else 1
+
         if self.archivo:
             self.estado_archivo = 'adjunto'
         else:
             self.estado_archivo = 'no adjunto'
+        
+        if self.archivo and self.archivo.name.lower().endswith('.pdf'):
+            try:
+                self.archivo.seek(0)
+                reader = PdfReader(self.archivo)
+                self.nro_fojas = len(reader.pages)
+            except Exception as e:
+                print(f"Error leyendo PDF: {e}")
+                self.nro_fojas = 0
+
         super().save(*args, **kwargs)
 
     def __str__(self):
